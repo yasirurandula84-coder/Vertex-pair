d81ca342e39c448c76b29c95dae652b87df8d947
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-03-25 22:01:36 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'yasirurandula84@gmail.com', 
+    password: 'Randula123yasiru',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
