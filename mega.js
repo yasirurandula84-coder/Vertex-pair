aeb56dccd242de17559b0a977577f85dc6a10ba0
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-03-26 17:04:51 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,22 +1,55 @@
-const mega = require("megajs");
-const auth = {
-    email: 'yasirurandula84@gmail.com', 
-    password: 'Randula123yasiru',
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
-}
 
-const upload = (data, name) => {
+import * as mega from "megajs";
+import fs from "fs";
+
+// Mega authentication credentials
+const auth = {
+    email: "yasirurandula84@gmail.com", // your mega account login email
+    password: "Randula123yasiru", // your mega account login password
+    userAgent:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+}; 
+
+export const upload = (filePath, fileName) => {
     return new Promise((resolve, reject) => {
         try {
-            const storage = new mega.Storage(auth, () => {
-                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
-                storage.on("add", (file) => {
+            const storage = new mega.Storage(auth, (err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                const readStream = fs.createReadStream(filePath);
+
+                const uploadStream = storage.upload({
+                    name: fileName,
+                    allowUploadBuffering: true,
+                });
+
+                readStream.pipe(uploadStream);
+
+                uploadStream.on("complete", (file) => {
                     file.link((err, url) => {
-                        if (err) throw err;
-                        storage.close()
-                        resolve(url);
+                        if (err) {
+                            reject(err);
+                        } else {
+                            storage.close();
+                            resolve(url);
+                        }
                     });
                 });
+
+                uploadStream.on("error", (error) => {
+                    reject(error);
+                });
+
+                readStream.on("error", (error) => {
+                    reject(error);
+                });
+            });
+
+            storage.on("error", (error) => {
+                reject(error);
             });
         } catch (err) {
             reject(err);
@@ -24,4 +57,28 @@ const upload = (data, name) => {
     });
 };
 
-module.exports = { upload };
+export const download = (url) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const file = mega.File.fromURL(url);
+
+            file.loadAttributes((err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                file.downloadBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                });
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+    
