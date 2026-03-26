bd544c8ed5e35c5675a736769ed9942a4495bfdc
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-03-26 19:22:32 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -5,7 +5,7 @@ import fs from "fs";
 // Mega authentication credentials
 const auth = {
     email: "yasirurandula84@gmail.com", // your mega account login email
-    password: "Randula123yasiru", // your mega account login password
+    password: "Randula123.", // your mega account login password
     userAgent:
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 }; 
