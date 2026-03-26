9a0bfe75a3b866c2d6b3afd73a1ab0904dd547b5
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-03-26 17:03:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-const express = require('express');
-const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
 
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
+import express from "express";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import path from "path";
+
+import pairRouter from "./pair.js";
+import qrRouter from "./qr.js";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 8000;
+
+import("events").then((events) => {
+    events.EventEmitter.defaultMaxListeners = 500;
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`⏩ Server running on http://localhost:` + PORT)
-})
+app.use(express.static(__dirname));
 
-module.exports = app
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pair.html"));
+});
+
+app.use("/pair", pairRouter);
+app.use("/qr", qrRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+export default app;
+
